abd4ee9eae04e052db20dcca403ab5d4250f0c2a
Stepan <usb-1999@mail.ru>
2021-05-21 22:55:02 +0600

    add_slider_dot

--- js/script.js
+++ js/script.js
@@ -281,7 +281,24 @@ window.addEventListener("DOMContentLoaded", () => {
         totalNumder = document.querySelector('#total'),
         sliderWrapper = document.querySelector('.offer__slider-wrapper'),
         innerWrapper = document.querySelector('.slider__width'),
-        width = window.getComputedStyle(sliderWrapper).width;
+        width = window.getComputedStyle(sliderWrapper).width,
+        slider =document.querySelector('.offer__slider');
+
+  const carousel = document.createElement('div');
+  carousel.classList.add('carousel-indicators');
+  const arrIndicator = [];
+
+  slider.style.position = 'relative';
+  slider.prepend(carousel);
+
+  for (let i = 0; i < slide.length; i++){
+    arrIndicator[i] = document.createElement('div');
+    arrIndicator[i].classList.add('dot');
+  }
+
+  arrIndicator.forEach((item,i) => {
+    carousel.append(item);
+  });      
   
   let offset = 0;
   let y = 0;
@@ -297,6 +314,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   currentNumder.textContent = `0${y + 1}`;
   totalNumder.textContent = `0${slide.length}`;
+  arrIndicator[0].style.opacity = '1';
  
   nextSlider.addEventListener('click', () => {
     y++;
@@ -306,9 +324,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }else{
       offset += +(width.slice(0, slide.length - 1)); 
     }
-    innerWrapper.style.transform = `translateX(-${offset}px)`;
-    currentNumder.textContent = `0${y + 1}`;
-   
+    setSlider(y, offset);
   });
 
   prevSlider.addEventListener("click", () => {
@@ -318,11 +334,40 @@ window.addEventListener("DOMContentLoaded", () => {
       y = slide.length - 1;
     }else{
       offset -= +(width.slice(0, slide.length - 1));
-      
     }
-    innerWrapper.style.transform = `translateX(-${offset}px)`;
-    currentNumder.textContent = `0${y + 1}`;
+    setSlider(y, offset);
   });
+
+  carousel.addEventListener('click', (e) => {
+    if(e.target && e.target.classList.contains('dot')){
+     arrIndicator.forEach((item, i) => {
+       if (e.target == item) {
+         y = i;
+         offset = +(width.slice(0, slide.length - 1)) * i;
+         setSlider(y, offset);
+        }
+      });
+    }
+  });
+
+  
+
+  function setSlider(num, offset) {
+    currentNumder.textContent = `0${num+ 1}`;
+    innerWrapper.style.transform = `translateX(-${offset}px)`;
+    arrIndicator.forEach((item, i) => {
+      item.style.opacity = '0.5';
+    });
+    arrIndicator[num].style.opacity = '1';
+  }
+
+  
+
+
+
+
+
+ 
 
   /*function hideSlide() {
     slide.forEach((item) => {
@@ -369,8 +414,7 @@ window.addEventListener("DOMContentLoaded", () => {
   });*/
 });
 
-
-
+//То что нажато меняем стиль, остальные нет
 
 
 
